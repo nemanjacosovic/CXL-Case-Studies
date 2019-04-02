dfd95df86cecb74443aab6e1dfe6ec3061201c64
Nemanja Cosovic <ncosovic@gmail.com>
2019-04-02 04:13:14 +0200

    Update to thumbnails and responsive layout
    # yellow bg on hover - removed, now it is only gray
    # thumb to be clickable as a whole - now works through JS, whoever is editing the files needs to set the correct URLS for the thumbnails in the anchor tag (‘View case study >’).
    # optimised the JS code
    # header menu - now working (only when clicking on the button itself)
    # small responsive update
    # smoothed image edging on the “Get Payroll Pricing Specific to Your Business”
    # added minified version of the main CSS file (main-min.css)

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Thumbnail
+const thumbMachine = (thumbArr) => {
+    thumbArr.forEach( thumb => {
+        thumb.addEventListener('click', () => {
+            const thumbChilds = thumb.childNodes
+            const redirectUrl = thumbChilds[7].getAttribute('href')
+
+            return window.location.href = redirectUrl
+        })
+    })
+}
+
+// Mobile menu
+const btnMenu = document.querySelector('#menu-button')
+const container = document.querySelector('#container')
+
+btnMenu.addEventListener('click', () => {
+    const ccl = container.classList
+
+    if (ccl.contains('header-visible')) {
+        ccl.remove('header-visible')
+    } else {
+        ccl.add('header-visible')
+    }
+})
+
+console.log('loaded')
